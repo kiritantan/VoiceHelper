6cbd45587daa0658d7636caca5e50f02a3e5ed91
kiri <kiri@beru.local>
2015-05-09 01:08:59 +0900

    implement editTextView function

--- speak/HomeviewController.swift
+++ speak/HomeviewController.swift
@@ -8,11 +8,14 @@
 
 import UIKit
 
-class HomeviewController: UIViewController {
+class HomeviewController: UIViewController,UITextViewDelegate {
 
+    @IBOutlet var textView: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        initTextView()
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +24,28 @@ class HomeviewController: UIViewController {
     }
 
 
+    func initTextView() {
+        textView.delegate = self
+        textView.userInteractionEnabled = false
+        textView.returnKeyType = UIReturnKeyType.Done
+    }
+    
+    func textView(textView: UITextView, shouldChangeTextInRange range: NSRange, replacementText text: String) -> Bool {
+        if text != "\n" {
+            return true
+        }
+        textView.resignFirstResponder()
+        return false
+    }
+    
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+        self.textView.resignFirstResponder()
+    }
+    
+    @IBAction func didTapEditTextViewButton(sender: AnyObject) {
+        self.textView.becomeFirstResponder()
+    }
+    
+    
 }
 
